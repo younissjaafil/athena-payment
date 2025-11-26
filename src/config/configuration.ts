a636a6fab4749c05bb114114c6f69592dd94a85e
Younissjaafil <youniss@3vo.me>
2025-11-26 22:37:52 +0200

    remove unncessary files

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,16 +1,16 @@
 export default () => ({
   app: {
-    port: parseInt(process.env.PORT || '3000', 10),
-    environment: process.env.NODE_ENV || 'development',
-    apiPrefix: process.env.API_PREFIX || 'api',
+    port: parseInt(process.env.PORT || '4000', 10),
+    environment: process.env.NODE_ENV,
+    apiPrefix: process.env.API_PREFIX,
   },
   payment: {
-    serviceUrl: process.env.PAYMENT_SERVICE_URL || 'https://api.whish.money',
-    channel: process.env.WHISH_CHANNEL || '',
-    secret: process.env.WHISH_SECRET || '',
-    websiteUrl: process.env.WHISH_WEBSITE_URL || '',
+    serviceUrl: process.env.PAYMENT_SERVICE_URL,
+    channel: process.env.WHISH_CHANNEL,
+    secret: process.env.WHISH_SECRET,
+    websiteUrl: process.env.WHISH_WEBSITE_URL,
   },
   gateway: {
-    baseUrl: process.env.GATEWAY_BASE_URL || 'https://payment.athena-ai.pro',
+    baseUrl: process.env.GATEWAY_BASE_URL,
   },
 });
